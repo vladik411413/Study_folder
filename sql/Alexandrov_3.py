0555465550bbc0f64fcab41842cc8e646197f453
Vladislav Alexandrov <ava015@campus.mephi.ru>
2023-11-18 16:31:26 +0300

    done
    Signed-off-by: Vladislav Alexandrov <ava015@campus.mephi.ru>

--- sql/Alexandrov_3.py
+++ sql/Alexandrov_3.py
@@ -54,36 +54,29 @@ class SqlConnect:
 			print("Ошибка при работе с sqlite: ", error)
 
 		finally:
-			
+			#Соединение с SQLite закрыто
 			if (sqlite_connection):
 				cur.close()
 				sqlite_connection.close()
-				#Соединение с SQLite закрыто
+				
 
 		
 Db = SqlConnect('sqlite_python.db')
 
 #creation of tables:
-if(Db.request("SELECT COUNT(1) FROM sqlite_master") == 0):
+if(Db.request("SELECT COUNT(1) FROM sqlite_master")[0][0] == 0):
 	Db.write("CREATE TABLE Students(id, name, surname, age, city);")
 	Db.write("CREATE TABLE Courses(id, name, time_start, time_end);")
 	Db.write("CREATE TABLE Student_courses(student_id,course_id, FOREIGN KEY (student_id) REFERENCES Students (id), FOREIGN KEY (course_id) REFERENCES Courses (id));")
-
-#adding data into tables
-if(Db.request("SELECT COUNT(1) FROM Courses") == 0):
 	Db.write("INSERT INTO Courses VALUES (1, 'python', '21.07.21', '21.08.21'), (2, 'java', '13.07.21', '16.08.21');")
-if(Db.request("SELECT COUNT(1) FROM Students") == 0):
 	Db.write("INSERT INTO Students VALUES (1, 'Max', 'Brooks', 24, 'Spb'), (2, 'John', 'Stones', 15, 'Spb'), (3, 'Andy', 'Wings', 45, 'Manhester'), (4, 'Kate', 'Brooks', 34, 'Spb');")
-if(Db.request("SELECT COUNT(1) FROM Student_courses") == 0):
 	Db.write("INSERT INTO Student_courses VALUES (1, 1),(2, 1),(3, 1),(4, 2);")
 
-#request
-
 #1. Всех студентов старше 30 лет.
-print(Db.request("SELECT name FROM Students WHERE age > 30;"))
+print("Всех студентов старше 30 лет. ",Db.request("SELECT name FROM Students WHERE age > 30;"))
 
 #2. Всех студентов, которые проходят курс по python.
-print(Db.request("SELECT name FROM Students WHERE id IN (SELECT student_id FROM Student_courses WHERE course_id IN (SELECT id FROM Courses WHERE name = 'python'));"))
+print("Всех студентов, которые проходят курс по python. ",Db.request("SELECT name FROM Students WHERE id IN (SELECT student_id FROM Student_courses WHERE course_id IN (SELECT id FROM Courses WHERE name = 'python'));"))
 
 #3. Всех студентов, которые проходят курс по python и из Spb.
-print(Db.request("SELECT name FROM Students WHERE id IN (SELECT student_id FROM Student_courses WHERE course_id IN (SELECT id FROM Courses WHERE name = 'python')) AND city = 'Spb';"))
+print("Всех студентов, которые проходят курс по python и из Spb. ",Db.request("SELECT name FROM Students WHERE id IN (SELECT student_id FROM Student_courses WHERE course_id IN (SELECT id FROM Courses WHERE name = 'python')) AND city = 'Spb';"))
